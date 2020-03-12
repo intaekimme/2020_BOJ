7baad8bad8bc1dce3e4163f1f3fad7eccad97e67
In Tae Kim <kit938639@gmail.com>
2020-03-12 19:55:53 +0900

    2. 틀렸습니다

--- 2020_03_12/1152.c
+++ 2020_03_12/1152.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #define MAX 1000000
 
@@ -5,12 +6,26 @@ int main() {
 	char str[MAX] = { 0, };
 	int i = 0;
 	int cnt = 0;
+	int len = 0;
 
 	fgets(str, MAX, stdin);
 
 	for (i = 0; str[i] != 0; i++) {
+		len++;
+	}
+
+
+	for (i = 0; str[i] != 0; i++) {
 		if (str[i] == 32) {
-			cnt++;
+			if (i == 0 && str[i] == 32) {
+				continue;
+			}
+			if (i == len - 2 && str[i] == 32) {
+				continue;
+			}
+			else {
+				cnt++;
+			}
 		}
 	}
 
