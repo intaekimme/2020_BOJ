1d95165f7a97bb147f727b576b9100385956a89d
In Tae Kim <kit938639@gmail.com>
2020-03-12 21:37:41 +0900

    4. 맞았습니다

--- 2020_03_12/1152.c
+++ 2020_03_12/1152.c
@@ -1,12 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#define MAX 1000000
+#define MAX 1000002
 
 int main() {
 	char str[MAX] = { 0, };
 	int i = 0;
-	int cnt = 0;
-	int len = 0;
+	int tap = 0;
+	int word = 0;
+	int len = -1;
 
 	fgets(str, MAX, stdin);
 
@@ -16,36 +17,24 @@ int main() {
 
 	for (i = 0; str[i] != 0; i++) {
 		if (str[i] == 32) {
-			if (i == 0 && str[i] == 32) {
-				continue;
-			}
-			else if (i == len - 2 && str[i] == 32) {
-				continue;
-			}
-			else {
-				cnt++;
-			}
+			tap++;
 		}
 	}
 
-	//공백 하나만 입력받았을 때 : len = 2, cnt = 0 >>> 0
-	//문자 1개만 입력받았을 때 : len = 2, cnt = 0 >>> 1
-	//공백 하나, 단어 하나 : len > 2, cnt = 0; >>> 1
-	//그 외
-	if (str[0] == 32 && len == 2 && cnt == 0) {
-		printf("%d\n", cnt);
-	}
-	else if (str[0] != 32 && len == 2 && cnt == 0) {
-		cnt++;
-		printf("%d\n", cnt);
-	}
-	else if (len > 2 && cnt == 0) {
-		cnt++;
-		printf("%d\n", cnt);
+	word = tap + 1;
+
+	if (len == tap) {
+		word = 0;
+		printf("%d\n", word);
 	}
 	else {
-		cnt++;
-		printf("%d\n", cnt);
+		if (str[0] == 32) {
+			word--;
+		}
+		if (str[len - 1] == 32) {
+			word--;
+		}
+		printf("%d\n", word);
 	}
 
 	return 0;
